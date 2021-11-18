f5b5333d9b384b13d138aacab4970df2713bf728
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-11-18 07:37:56 -0500

    Starting project

---
+++ src/test/java/com/hackaton/foodforall/FoodforallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.foodforall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodforallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
