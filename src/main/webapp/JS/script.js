e64ab234bd5141316b55ec330f38e691586984f7
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-11-18 16:32:46 -0500

    Updating forms

--- src/main/webapp/JS/script.js
+++ src/main/webapp/JS/script.js
@@ -1,3 +1,24 @@
 /**
- * Javascript Document to 
+ * Javascript Document to handle window changes
  */
+
+/**
+ * Variables that catch HTML elements
+ */
+const links = document.querySelectorAll('#menu a');
+const mainCont = document.getElementById('mainCont');
+const formXHTTP = new XMLHttpRequest();
+
+links.forEach((link) => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        let id = e.target.getAttribute("id");
+        console.log(id);
+        formXHTTP.open('GET', id + '.html', true);
+        formXHTTP.send();
+    });
+});
+
+formXHTTP.onload = function () {
+    mainCont.innerHTML = this.responseText;
+}
