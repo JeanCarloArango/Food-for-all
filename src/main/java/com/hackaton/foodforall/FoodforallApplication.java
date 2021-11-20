178e0c0adf45662b507ffb512eb861792534b989
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-11-20 10:04:08 -0500

    App Name

--- src/main/java/com/hackaton/foodforall/FoodforallApplication.java
+++ src/main/java/com/hackaton/foodforall/FoodforallApplication.java
@@ -2,11 +2,13 @@ package com.hackaton.foodforall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodforallApplication {
+public class FoodforallApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/BraveTeamApp");
 		SpringApplication.run(FoodforallApplication.class, args);
 	}
 
